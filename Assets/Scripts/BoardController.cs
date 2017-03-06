3a2d0e778be56943ef220fe979f73f4834e4bf19
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-06 09:51:54 +0100

    control of board, ball dont move

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour {
+
+    [SerializeField]
+    Transform outerControl;
+
+    [SerializeField]
+    Transform innerControl;
+
+    [SerializeField, Range(0, 15)]
+    float rotationMagnitude;
+
+    [SerializeField]
+    Vector3 outerEulerAxis = new Vector3(1, 0, 0);
+
+    [SerializeField]
+    Vector3 innerEulerAxis = new Vector3(0, 1, 0);
+
+    float innerRotation;
+
+    float outerRotation;
+
+    [SerializeField, Range(0, 1)]
+    float delay; 
+
+	void Update () {
+        float innerTarget = Input.GetAxis("Horizontal");
+        float outerTarget = Input.GetAxis("Vertical");
+
+        innerRotation = Mathf.Lerp(innerTarget * rotationMagnitude, innerRotation, delay);
+        outerRotation = Mathf.Lerp(outerTarget * rotationMagnitude, outerRotation, delay);
+
+        outerControl.localEulerAngles = outerEulerAxis * outerRotation;
+        innerControl.localEulerAngles = innerEulerAxis * innerRotation;
+    }
+
+    public Vector3 Slope
+    {
+        get
+        {
+            return new Vector3(outerEulerAxis.y, outerEulerAxis.x) * rotationMagnitude * outerRotation + 
+                new Vector3(innerEulerAxis.y, innerEulerAxis.x) * rotationMagnitude * innerRotation;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    BoardController board;
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(board.Slope);
+    }
+}
