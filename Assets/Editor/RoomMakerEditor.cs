dc31e065885e48de8c5134fdbe21134d0d4968e0
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-07 01:06:29 +0100

    Room maker started

---
+++ Assets/Editor/RoomMakerEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RoomMaker))]
+public class RoomMakerEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Generate Rooms"))
+        {
+            (target as RoomMaker).GenerateRooms();
+        }
+    }
+}
---
+++ Assets/Scripts/RoomMaker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMaker : MonoBehaviour {
+
+    [SerializeField]
+    BallPath ballPath;
+
+    [SerializeField]
+    BoardGrid boardGrid;
+
+    public void GenerateRooms()
+    {
+
+    }
+}
