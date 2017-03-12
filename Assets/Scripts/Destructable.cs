5dd41e5bedacf29181591060351c7751dfb3cb25
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-12 10:13:34 +0100

    starting destruction and attack

---
+++ Assets/Scripts/Destructable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour {
+
+    [SerializeField]
+    float health;
+
+    Rigidbody rb;
+
+    [SerializeField]
+    float velocityForcePower = 1;
+
+
+    float VelocityForce
+    {
+        get
+        {
+            if (rb == null)
+            {
+                return 0;
+            }
+            return Mathf.Pow(rb.velocity.magnitude, velocityForcePower) * rb.mass;
+        }
+    }
+
+    public int GetVelocityForce()
+    {
+        return Mathf.Clamp(Mathf.FloorToInt(VelocityForce), 0, MaxVelocityForce);
+    }
+
+    public int MaxVelocityForce = 10;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log(LayerMask.LayerToName(collision.gameObject.layer));
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("walls"))
+        {
+            Debug.Log(VelocityForce);
+        }
+    }
+}
---
+++ Assets/Scripts/VelForceUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VelForceUI : MonoBehaviour {
+
+    [SerializeField]
+    Destructable playerDestructable;
+
+    Text tField;
+    
+	void Start () {
+        tField = GetComponent<Text>();	
+	}
+	
+	void Update () {
+        int velForce = playerDestructable.GetVelocityForce();
+        int maxVelForce = playerDestructable.MaxVelocityForce;
+
+        string s = "";
+        for (int i = 0; i<maxVelForce; i++)
+        {
+            if (i < velForce)
+            {
+                s += "#";
+            } else
+            {
+                s += " ";
+            }
+        }
+        tField.text = string.Format("[{0}]", s);
+	}
+}
