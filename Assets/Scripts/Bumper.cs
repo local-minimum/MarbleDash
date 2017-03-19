a54dfae4c3d5a45f63507b5e3947bf908e595580
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-19 12:44:40 +0100

    bumper prefab

---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour {
+
+    [SerializeField]
+    Animator anim;
+
+    [SerializeField]
+    string bumpAnim = "Bump";
+
+    [SerializeField]
+    float bumpForce = 10;
+
+    [SerializeField]
+    SphereCollider bumpRangeTrigger;
+
+    [SerializeField]
+    bool bumpOnTurn;
+
+    int playerLayer;
+
+    private void Awake()
+    {
+        playerLayer = LayerMask.NameToLayer("player");
+    }
+
+    Level lvl;
+
+    private void OnEnable()
+    {
+        lvl = Level.instance;
+        lvl.OnTurnTick += Instance_OnTurnTick;
+
+    }
+
+    private void OnDisable()
+    {
+        lvl.OnTurnTick -= Instance_OnTurnTick;
+    }
+
+    private void Instance_OnTurnTick(PlayerController player, float tickTime)
+    {
+        if (playerRB && bumpOnTurn)
+        {
+            StartCoroutine(BumpIt(playerRB));
+        }
+    }
+
+    IEnumerator<WaitForSeconds> BumpIt(Rigidbody playerRB)
+    {
+        anim.SetTrigger(bumpAnim);
+        yield return new WaitForSeconds(0.1f);
+        playerRB.AddExplosionForce(bumpForce, transform.position, bumpRangeTrigger.radius * 2);
+    }
+
+    Rigidbody playerRB;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == playerLayer)
+        {
+            playerRB = other.GetComponent<Rigidbody>();
+            if (!bumpOnTurn)
+            {
+                StartCoroutine(BumpIt(playerRB));
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>() == playerRB)
+        {
+            playerRB = null;
+        }
+    }
+}
