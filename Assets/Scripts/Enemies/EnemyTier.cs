5612448044df986bda6472f5e1be1135d7fabad9
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-23 00:29:50 +0100

    draft of enemy settings available to define enemy tiers

--- Assets/Scripts/Enemies/EnemyTier.cs
+++ Assets/Scripts/Enemies/EnemyTier.cs
@@ -9,8 +9,17 @@ public class EnemyTier {
 
     public int startHealth;
 
+    //Consider each attack type have an attack value range
+    //Need as many as the included attack modes in the enum
     public int[] minAttack;
     public int[] maxAttack;
 
+    //Behaviour priority weights
+    public float[] behaviourProbWeights;
+
+    //Behaviour cooldowns
+    public int[] behaviourCoolDowns;
+
+    //List of behaviours to select from each turn
     public EnemyMode[] availableModes;
 }
