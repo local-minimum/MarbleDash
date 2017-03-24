0266ac776690913cac6704021899c4359571dd60
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-24 22:46:13 +0100

    Considering starting addon for generic stuff

---
+++ Assets/Addon/LocalMinimum/Boolean/ArrayRegions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LocalMinimum.Boolean
+{
+    public static class ArrayRegions
+    {
+        public static int[,] Label(this bool[,] input)
+        {
+            int w = input.GetLength(0);
+            int h = input.GetLength(1);
+
+            int[,] labels = new int[w, h];
+            int labelCount = 0;
+            for (int x=0; x<w; x++)
+            {
+                for (int y=0; y<h; y++)
+                {
+                    if (!input[x, y] || labels[x, y] > 0)
+                    {
+                        continue;
+                    }
+
+                    labelCount++;
+
+                    List<KeyValuePair<int, int>> queue = new List<KeyValuePair<int, int>>();
+
+                    labels[x, y] = labelCount;
+                    queue.Add(new KeyValuePair<int, int>(x, y));
+
+                    while (queue.Count > 0)
+                    {
+
+                        KeyValuePair<int, int> cur = queue[0];
+                        queue.RemoveAt(0);
+
+                    }
+                }
+            }
+
+            return labels;
+        }
+    }
+}
