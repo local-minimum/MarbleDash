d4c1abaaf260c882753fc6fa539a9938d312e7e4
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-25 20:27:30 +0100

    preparing for path finding

--- Assets/Addon/LocalMinimum/Grid/GridPath.cs
+++ Assets/Addon/LocalMinimum/Grid/GridPath.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace LocalMinimum.Grid
 {
     public static class GridPath
     {
 
-        public static List<GridPos> FindPath(GridPos source, GridPos targer, int[,] array, int filter)
+        public static List<GridPos> FindPath(GridPos source, GridPos targert, int[,] array, int filter)
         {
+            bool searching = source == targert;
             List<GridPos> path = new List<GridPos>();
-            Dictionary<GridPos, KeyValuePair<int, List<GridPos>>> paths = new Dictionary<GridPos, KeyValuePair<int, List<GridPos>>>();
+            Dictionary<GridPos, KeyValuePair<int, List<GridPos>>> explored = new Dictionary<GridPos, KeyValuePair<int, List<GridPos>>>();
+            Dictionary<GridPos, KeyValuePair<int, List<GridPos>>> seen = new Dictionary<GridPos, KeyValuePair<int, List<GridPos>>>();
+            GridPos cur = source;
 
-            path.Add(source);
+            while (searching) {
+                List<GridPos> curPath = new List<GridPos>();
+                curPath.Add(cur);
+                explored.Add(source, new KeyValuePair<int, List<GridPos>>(0, curPath));
 
+                //TODO: For each neighbour, if valid add
+                
+            }
 
             return path;
         }
