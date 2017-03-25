8847f5d7795e89a39d357b1f377c1a5554502c12
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-25 20:17:33 +0100

    Considering adding path finding

---
+++ Assets/Addon/LocalMinimum/Grid/GridPath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LocalMinimum.Grid
+{
+    public static class GridPath
+    {
+
+        public static List<GridPos> FindPath(GridPos source, GridPos targer, int[,] array, int filter)
+        {
+            List<GridPos> path = new List<GridPos>();
+            Dictionary<GridPos, KeyValuePair<int, List<GridPos>>> paths = new Dictionary<GridPos, KeyValuePair<int, List<GridPos>>>();
+
+            path.Add(source);
+
+
+            return path;
+        }
+
+        public static bool IsInside(GridPos pos, int[,] array)
+        {
+            return pos.x >= 0 && pos.y >= 0 && pos.x < array.GetLength(0) && pos.y < array.GetLength(1);
+        }
+        
+        public static bool IsInRegion(GridPos pos, int[,] array, int filter)
+        {
+            return IsInside(pos, array) && array[pos.x, pos.y] == filter;
+        }
+    }
+}
