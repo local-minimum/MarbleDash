c49a313c6beb654b570ce13368cf6ad355627efb
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-27 21:12:27 +0200

    Adding functions to array class

--- Assets/Addon/LocalMinimum/Array/ArrayRegions.cs
+++ Assets/Addon/LocalMinimum/Array/ArrayRegions.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 namespace LocalMinimum.Boolean
 {
+    public struct Coordinate
+    {
+        public int x;
+        public int y;
+
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public static class ArrayRegions
     {
         public static int[,] Label(this bool[,] input, out int labelCount)
@@ -13,9 +25,9 @@ namespace LocalMinimum.Boolean
 
             int[,] labels = new int[w, h];
             labelCount = 0;
-            for (int x=0; x<w; x++)
+            for (int x = 0; x < w; x++)
             {
-                for (int y=0; y<h; y++)
+                for (int y = 0; y < h; y++)
                 {
                     if (!input[x, y] || labels[x, y] > 0)
                     {
@@ -59,6 +71,215 @@ namespace LocalMinimum.Boolean
             return labels;
         }
 
+        public static int[,] DistanceToEgde(this bool[,] filter, bool treatEdgeAsBorder = false)
+        {
+            return filter.Distance(filter.Edge(treatEdgeAsBorder).ToCoordinates());
+        }
 
-    }
+        public static int[,] Distance(this bool[,] filter, List<Coordinate> sources)
+        {
+            int w = filter.GetLength(0);
+            int h = filter.GetLength(1);
+            int lastX = w - 1;
+            int lastY = h - 1;
+
+            int[,] distances = Fill(w, h, -1);
+
+            int index = 0;
+            int length = sources.Count;
+
+            while (index < length)
+            {
+                Coordinate coord = sources[index];
+                int x = coord.x;
+                int y = coord.y;
+                int lowest = -1;
+
+                if (x != 0 && filter[x - 1, y])
+                {
+                    if (distances[x - 1, y] < 0)
+                    {
+                        sources.Add(new Coordinate(x - 1, y));
+                        length++;
+                    }
+                    else 
+                    {
+                        lowest = Mathf.Min(lowest, distances[x - 1, y]);
+                    }
+                }
+
+                if (x != lastX && filter[x + 1, y])
+                {
+                    if (distances[x + 1, y] < 0)
+                    {
+                        sources.Add(new Coordinate(x + 1, y));
+                        length++;
+                    }
+                    else
+                    {
+                        lowest = Mathf.Min(lowest, distances[x + 1, y]);
+                    }
+                }
+
+                if (y != 0 && filter[x, y - 1])
+                {
+                    if (distances[x, y - 1] < 0) {
+                        sources.Add(new Coordinate(x, y - 1));
+                        length++;
+                    }
+                    else {
+                        lowest = Mathf.Min(lowest, distances[x, y - 1]);
+                    }
+                    
+                }
+
+                if (y != lastY && filter[x, y + 1]) {
+                    if (distances[x, y + 1] < 0)
+                    {
+                        sources.Add(new Coordinate(x, y + 1));
+                        length++;
+                    }
+                    else
+                    {
+                        lowest = Mathf.Min(lowest, distances[x, y + 1]);
+                    }
+                }
+
+                distances[x, y] = lowest + 1;
+                index++;
+            }
+
+            return distances;
+        }
+
+        public static bool[,] Edge(this bool[,] input, bool treatEdgeAsBorder = false)
+        {
+            int w = input.GetLength(0);
+            int h = input.GetLength(1);
+            int lastX = w - 1;
+            int lastY = h - 1;
+            bool[,] output = new bool[w, h];
+
+            for (int x = 0; x < w; x++)
+            {
+                for (int y = 0; y < h; y++)
+                {
+                    if (x == 0 || y == 0 || x == lastX || y == lastY)
+                    {
+                        if (treatEdgeAsBorder)
+                        {
+                            output[x, y] = true;
+                        }
+                        else
+                        {
+                            bool center = input[x, y];
+                            if (x > 0 && input[x - 1, y] != center ||
+                                x  < lastX && input[x + 1, y] != center ||
+                                y > 0 && input[x, y - 1] != center ||
+                                y < lastY && input[x, y + 1] != center)
+                            {
+                                output[x, y] = true;
+                            }
+                        }
+                        
+                        continue;
+                    } else
+                    {
+                        bool center = input[x, y];
+                        if (input[x - 1, y] != center || 
+                            input[x + 1, y] != center || 
+                            input[x, y - 1] != center || 
+                            input[x, y + 1] != center)
+                        {
+                            output[x, y] = true;
+                        }
+
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        public static bool[,] Equals(this int[,] input, int value)
+        {
+            int w = input.GetLength(0);
+            int h = input.GetLength(1);
+            bool[,] output = new bool[w, h];
+
+            for (int x=0; x<w; x++)
+            {
+                for (int y=0; y<h; y++)
+                {
+                    if (input[x, y] == value)
+                    {
+                        output[x, y] = true;
+                    }
+                }
+            }
+            return output;
+        }
+
+        public static List<Coordinate> ToCoordinates(this bool[,] input)
+        {
+            List<Coordinate> coordinates = new List<Coordinate>();
+            int w = input.GetLength(0);
+            int h = input.GetLength(1);
+
+            for (int x = 0; x < w; x++)
+            {
+                for (int y = 0; y < h; y++)
+                {
+                    if (input[x, y])
+                    {
+                        coordinates.Add(new Coordinate(x, y));
+                    }
+                }
+            }
+
+            return coordinates;
+        }
+
+        public static int[,] Fill(int w, int h, int value)
+        {
+            return new int[w, h].Fill(value);
+        }
+
+        public static int[,] Fill(this int[,] input, int value)
+        {
+            int w = input.GetLength(0);
+            int h = input.GetLength(1);
+
+            for (int x=0; x<w; x++)
+            {
+                for (int y=0; y<h; y++)
+                {
+                    input[x, y] = value;
+                }
+            }
+            return input;
+        }
+
+        public static bool[,] Invert(this bool[,] input)
+        {
+            int w = 0;
+            int h = 0;
+            bool[,] inverted = new bool[w, h];
+            for (int x = 0; x < w; x++)
+            {
+                for (int y = 0; y < h; y++)
+                {
+
+                    if (!input[x, y])
+                    {
+                        inverted[x, y] = true;
+                    }
+                }
+
+            }
+
+            return inverted;
+        }
+
+     }
 }
