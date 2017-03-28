2e4c145d6531745bbacb697f4b2265bbda152ba0
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-28 17:51:17 +0200

    foot farting better structured for future export

--- Assets/Addon/LocalMinimum/Mesh/FootFarts.cs
+++ Assets/Addon/LocalMinimum/Mesh/FootFarts.cs
@@ -3,61 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using LocalMinimum.Mesh;
 
-public class FootFarts : MonoBehaviour {
+namespace LocalMinimum.Mesh
+{
 
-    [SerializeField]
-    Renderer fartTarget;
-
-    [SerializeField]
-    ParticleSystem ps;
-
-    [SerializeField]
-    int particles;
-
-    [SerializeField]
-    float emitProb = 0.5f;
-    
-    private void Awake()
+    public class FootFarts : MonoBehaviour
     {
-        if (fartTarget)
+
+        [SerializeField]
+        Renderer fartTarget;
+
+        [SerializeField]
+        ParticleSystem ps;
+
+        [SerializeField]
+        int particles;
+
+        [SerializeField]
+        float emitProb = 0.5f;
+
+        [SerializeField, HideInInspector]
+        MeshDataMinimal meshData = new MeshDataMinimal();
+
+        private void Awake()
         {
-            //Just to be sure for now
-            Material m = fartTarget.material;
-            fartTarget.material = Instantiate(m);
-        }
-    }
-
-    ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
-
-    private void OnCollisionStay(Collision collision)
-    {
-        ContactPoint cPoint = collision.contacts[0];
-        Renderer r = collision.gameObject.GetComponent<Renderer>();
-
-        Color c = GetUVColor(cPoint, r.material);
-
-        if (fartTarget)
-        {
-            fartTarget.material.color = c;
+            if (fartTarget)
+            {
+                //Just to be sure for now
+                Material m = fartTarget.material;
+                fartTarget.material = Instantiate(m);
+            }
         }
 
-        if (ps && Random.value < emitProb)
-        {
-            emitParams.startColor = c;
-            ps.Emit(emitParams, particles);
-        }
-    }
+        ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
 
-    Color GetUVColor(ContactPoint cPoint, Material mat)
-    {
-        Mesh m = cPoint.otherCollider.GetComponent<MeshFilter>().mesh;
-        Vector3 otherLocalPt = cPoint.otherCollider.transform.InverseTransformPoint(cPoint.point);
-        int[] closest = GeometryTools.GetTwoClosestVertsToPoint(m.vertices, otherLocalPt);
-        int bestTri = GeometryTools.GetClosestTriStartIndex(m.triangles, m.vertices, closest[0], closest[1], otherLocalPt);
-        Vector2 uvPos = GeometryTools.TranslateMeshPointToUV(m.triangles, m.uv, m.vertices, otherLocalPt, bestTri);
-        Texture2D tex = (mat.mainTexture as Texture2D);
-        return tex.GetPixel(Mathf.FloorToInt(uvPos.x * tex.width), Mathf.FloorToInt(uvPos.y * tex.height));
+        private void OnCollisionStay(Collision collision)
+        {
+            ContactPoint cPoint = collision.contacts[0];
+            Renderer r = collision.gameObject.GetComponent<Renderer>();
+
+            Color c = GetUVColor(cPoint, r.material);
+
+            if (fartTarget)
+            {
+                fartTarget.material.color = c;
+            }
+
+            if (ps && Random.value < emitProb)
+            {
+                emitParams.startColor = c;
+                ps.Emit(emitParams, particles);
+            }
+        }
+
+        Color GetUVColor(ContactPoint cPoint, Material mat)
+        {
+            
+            if (meshData.SyncData(cPoint.otherCollider.GetComponent<MeshFilter>().mesh))
+            {
+                //Accessed target, should have collided with new mesh or mesh changed
+            }
+            Vector3 otherLocalPt = cPoint.otherCollider.transform.InverseTransformPoint(cPoint.point);
+            int[] closest = GeometryTools.GetTwoClosestVertsToPoint(meshData.verts, otherLocalPt);
+            int bestTri = GeometryTools.GetClosestTriStartIndex(meshData.tris, meshData.verts, closest[0], closest[1], otherLocalPt);
+            Vector2 uvPos = GeometryTools.TranslateMeshPointToUV(meshData.tris, meshData.uv, meshData.verts, otherLocalPt, bestTri);
+            Texture2D tex = (mat.mainTexture as Texture2D);
+            return tex.GetPixel(Mathf.FloorToInt(uvPos.x * tex.width), Mathf.FloorToInt(uvPos.y * tex.height));
+        }
+
     }
 
 }
-
---
+++ Assets/Addon/LocalMinimum/Mesh/MeshData.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LocalMinimum.Mesh {
+
+    [System.Serializable]
+    public struct MeshDataMinimal {
+
+        public int[] tris;
+        public Vector3[] verts;
+        public Vector2[] uv;
+        public Vector2[] uv2;
+        public Vector2[] uv3;
+        public Vector2[] uv4;
+        bool createdFromMesh;
+        int meshId;
+        int meshHash;
+
+        public MeshDataMinimal(UnityEngine.Mesh m)
+        {
+            tris = m.triangles;
+            verts = m.vertices;
+            uv = m.uv;
+            uv2 = m.uv2;
+            uv3 = m.uv3;
+            uv4 = m.uv4;
+            meshId = m.GetInstanceID();
+            meshHash = m.GetHashCode();
+            createdFromMesh = true;
+
+        }
+
+        public bool SyncData(UnityEngine.Mesh m)
+        {
+            if (!createdFromMesh && m.GetInstanceID() != meshId && m.GetHashCode() != meshHash)
+            {
+                tris = m.triangles;
+                verts = m.vertices;
+                uv = m.uv;
+                uv2 = m.uv2;
+                uv3 = m.uv3;
+                uv4 = m.uv4;
+                meshId = m.GetInstanceID();
+                meshHash = m.GetHashCode();
+                createdFromMesh = true;
+
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
